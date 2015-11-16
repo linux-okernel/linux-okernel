c59aa56e4251f19668d4c59d1b5ac88c56855083
cid <cid@unix-1>
2015-11-16 19:47:27 +0000

    Hacked in rest of dune/kvm vmxon initialization code. Compiles, not tested.

--- arch/x86/okernel/vmx.c
+++ arch/x86/okernel/vmx.c
@@ -63,15 +63,21 @@
 #include <asm/vmx.h>
 #include <asm/unistd_64.h>
 #include <asm/virtext.h>
+#include <asm/percpu.h>
+#include <asm/paravirt.h>
 
 #include "vmx.h"
 
+#define write_cr4 native_write_cr4
+#define read_cr4 native_read_cr4
+
+
 static atomic_t vmx_enable_failed;
 
-static DECLARE_BITMAP(vmx_vpid_bitmap, VMX_NR_VPIDS);
-static DEFINE_SPINLOCK(vmx_vpid_lock);
+//static DECLARE_BITMAP(vmx_vpid_bitmap, VMX_NR_VPIDS);
+//static DEFINE_SPINLOCK(vmx_vpid_lock);
 
-static unsigned long *msr_bitmap;
+//static unsigned long *msr_bitmap;
 
 static DEFINE_PER_CPU(struct vmcs *, vmxarea);
 static DEFINE_PER_CPU(struct desc_ptr, host_gdt);
@@ -144,8 +150,136 @@ static inline bool cpu_has_vmx_ept_ad_bits(void)
 	return vmx_capability.ept & VMX_EPT_AD_BIT;
 }
 
+
+static struct vmcs *__vmx_alloc_vmcs(int cpu)
+{
+	int node = cpu_to_node(cpu);
+	struct page *pages;
+	struct vmcs *vmcs;
+
+	pages = alloc_pages_exact_node(node, GFP_KERNEL, vmcs_config.order);
+	if (!pages)
+		return NULL;
+	vmcs = page_address(pages);
+	memset(vmcs, 0, vmcs_config.size);
+	vmcs->revision_id = vmcs_config.revision_id; /* vmcs revision id */
+	return vmcs;
+}
+
+
+/**
+ * vmx_free_vmcs - frees a VMCS region
+ */
+static void vmx_free_vmcs(struct vmcs *vmcs)
+{
+	free_pages((unsigned long)vmcs, vmcs_config.order);
+}
+
+static inline void __vmxon(u64 addr)
+{
+	asm volatile (ASM_VMX_VMXON_RAX
+			: : "a"(&addr), "m"(addr)
+			: "memory", "cc");
+}
+
+static inline void __vmxoff(void)
+{
+	asm volatile (ASM_VMX_VMXOFF : : : "cc");
+}
+
+
+/**
+ * __vmx_enable - low-level enable of VMX mode on the current CPU
+ * @vmxon_buf: an opaque buffer for use as the VMXON region
+ */
+static __init int __vmx_enable(struct vmcs *vmxon_buf)
+{
+	u64 phys_addr = __pa(vmxon_buf);
+	u64 old, test_bits;
+
+	if (read_cr4() & X86_CR4_VMXE)
+		return -EBUSY;
+
+	rdmsrl(MSR_IA32_FEATURE_CONTROL, old);
+
+	test_bits = FEATURE_CONTROL_LOCKED;
+	test_bits |= FEATURE_CONTROL_VMXON_ENABLED_OUTSIDE_SMX;
+	if (tboot_enabled())
+		test_bits |= FEATURE_CONTROL_VMXON_ENABLED_INSIDE_SMX;
+
+	if ((old & test_bits) != test_bits) {
+		/* enable and lock */
+		wrmsrl(MSR_IA32_FEATURE_CONTROL, old | test_bits);
+	}
+	write_cr4(read_cr4() | X86_CR4_VMXE);
+
+	__vmxon(phys_addr);
+	//vpid_sync_vcpu_global();
+	//ept_sync_global();
+
+	return 0;
+}
+
+
+
+/**
+ * vmx_enable - enables VMX mode on the current CPU
+ * @unused: not used (required for on_each_cpu())
+ *
+ * Sets up necessary state for enable (e.g. a scratchpad for VMXON.)
+ */
+static __init void vmx_enable(void *unused)
+{
+	int ret;
+	struct vmcs *vmxon_buf = __this_cpu_read(vmxarea);
+
+	if ((ret = __vmx_enable(vmxon_buf)))
+		goto failed;
+
+	__this_cpu_write(vmx_enabled, 1);
+	native_store_gdt(this_cpu_ptr(&host_gdt));
+
+	printk(KERN_INFO "vmx: VMX enabled on CPU %d\n",
+	       raw_smp_processor_id());
+	return;
+
+failed:
+	atomic_inc(&vmx_enable_failed);
+	printk(KERN_ERR "vmx: failed to enable VMX, err = %d\n", ret);
+}
+
+/**
+ * vmx_disable - disables VMX mode on the current CPU
+ */
+static void vmx_disable(void *unused)
+{
+	if (__this_cpu_read(vmx_enabled)) {
+		__vmxoff();
+		write_cr4(read_cr4() & ~X86_CR4_VMXE);
+		__this_cpu_write(vmx_enabled, 0);
+	}
+}
+
+/**
+ * vmx_free_vmxon_areas - cleanup helper function to free all VMXON buffers
+ */
+static void vmx_free_vmxon_areas(void)
+{
+	int cpu;
+
+	for_each_possible_cpu(cpu) {
+		if (per_cpu(vmxarea, cpu)) {
+			vmx_free_vmcs(per_cpu(vmxarea, cpu));
+			per_cpu(vmxarea, cpu) = NULL;
+		}
+	}
+}
+
+
 int __init vmx_init(void)
 {
+	int r, cpu;
+	
         if (!cpu_has_vmx()) {
 		printk(KERN_ERR "vmx: CPU does not support VT-x\n");
 		return -EIO;
@@ -165,5 +299,35 @@ int __init vmx_init(void)
 		printk(KERN_ERR "vmx: ability to load EFER register is required\n");
 		return -EIO;
 	}
+
+	for_each_possible_cpu(cpu) {
+		struct vmcs *vmxon_buf;
+
+		vmxon_buf = __vmx_alloc_vmcs(cpu);
+		if (!vmxon_buf) {
+			vmx_free_vmxon_areas();
+			return -ENOMEM;
+		}
+
+		per_cpu(vmxarea, cpu) = vmxon_buf;
+	}
+
+	atomic_set(&vmx_enable_failed, 0);
+	if (on_each_cpu(vmx_enable, NULL, 1)) {
+		printk(KERN_ERR "vmx: timeout waiting for VMX mode enable.\n");
+		r = -EIO;
+		goto failed1; /* sadly we can't totally recover */
+	}
+
+	if (atomic_read(&vmx_enable_failed)) {
+		r = -EBUSY;
+		goto failed2;
+	}
         return 0;
+	
+failed2:
+	on_each_cpu(vmx_disable, NULL, 1);
+failed1:
+	vmx_free_vmxon_areas();
+	return r;
 }
