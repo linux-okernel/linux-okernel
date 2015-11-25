2b3655c44163d1984eef900b5a40f9765e707f5c
cid <cid@localhost>
2015-11-25 17:23:55 +0000

    Pull in notifier code (may not need it) and original dune/kvm ept code (again may use our original own ept setup code.

--- arch/x86/okernel/vmx.c
+++ arch/x86/okernel/vmx.c
@@ -478,7 +478,19 @@ static void vmcs_load(struct vmcs *vmcs)
 /*-------------------------------------------------------------------------------------*/
 /*  start: vmx__launch releated code                                                   */
 /*-------------------------------------------------------------------------------------*/
+static u64 construct_eptp(unsigned long root_hpa)
+{
+	u64 eptp;
 
+	/* TODO write the value reading from MSR */
+	eptp = VMX_EPT_DEFAULT_MT |
+		VMX_EPT_DEFAULT_GAW << VMX_EPT_GAW_EPTP_SHIFT;
+	if (cpu_has_vmx_ept_ad_bits())
+		eptp |= VMX_EPT_AD_ENABLE_BIT;
+	eptp |= (root_hpa & PAGE_MASK);
+
+	return eptp;
+}
 
 /**
  * vmx_alloc_vmcs - allocates a VMCS region
