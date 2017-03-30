dc16430a2c6a31a4ef45d1472ae85e1968973158
Nigel Edwards <nje@cos-03.uk.rdlabs.hpecorp.net>
2017-03-30 16:30:18 +0100

    Added debugging for tracking down rare case of when we get an EPT_violation with a valid virtual address, but no page table entry.

--- include/linux/okernel.h
+++ include/linux/okernel.h
@@ -102,11 +102,17 @@ int ok_free_protected_page(struct page *pg);
 #else
 #define HDEBUG(fmt, args...)
 #endif
-//#define HPL_DEBUG2
+//#define HPE_DEBUG2
 #ifdef HPE_DEBUG2
 #define HDEBUG2(fmt, args...) trace_printk( KERN_ERR "%s: cpu(%d) pid(%d) %s: " fmt, vmx_nr_mode()?"NR":"R ", raw_smp_processor_id(), current->pid,__func__, ## args)
 #else
 #define HDEBUG2(fmt, args...)
+#endif
+#define HPE_DEBUG3
+#ifdef HPE_DEBUG3
+#define HDEBUG3(fmt, args...) trace_printk( KERN_ERR "%s: cpu(%d) pid(%d) %s: " fmt, vmx_nr_mode()?"NR":"R ", raw_smp_processor_id(), current->pid,__func__, ## args)
+#else
+#define HDEBUG3(fmt, args...)
 #endif
 
 
