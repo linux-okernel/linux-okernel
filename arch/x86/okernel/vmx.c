304b83092d85363307bceb5fc017848c636b233e
Nigel Edwards <nje@cos-03.uk.rdlabs.hpecorp.net>
2017-05-08 18:48:02 +0100

    Intermediate debugging work to be thrown away when I understand what is going on!!!

--- arch/x86/okernel/vmx.c
+++ arch/x86/okernel/vmx.c
@@ -3904,6 +3904,7 @@ void vmx_handle_vmcall(struct vmx_vcpu *vcpu, int nr_irqs_enabled)
 		check_int_enabled(vcpu, "DOEXIT");
 		code = (long)vcpu->regs[VCPU_REGS_RBX];
 		HDEBUG("calling do_exit(%ld)...\n", code);
+		dump_log(vcpu);
 		vmx_destroy_vcpu(vcpu);
 		do_exit(code);
 	} else {
@@ -4052,6 +4053,20 @@ void flags_from_qual(unsigned long qual, unsigned long *s, unsigned long *c)
 	*c = ((~*s) & EPT_PERM_MASK);
 }
 
+void vmexit_loop_detect(struct vmx_vcpu *vcpu, unsigned long gpa)
+{
+	if (vcpu->lepa == gpa){
+		vcpu->ec++;
+		if (vcpu->ec >  10){
+			BUG();
+		}
+	} else {
+		vcpu->ec = 0;
+		vcpu->lepa = gpa;
+	}
+	return;
+}
+
 int page_walk_ept_viol(struct vmx_vcpu *vcpu, unsigned long gpa,
 		       unsigned long qual)
 {
@@ -4115,6 +4130,8 @@ int handle_EPT_violation(struct vmx_vcpu *vcpu)
 		unsigned long *epte, mapped, s_flags, c_flags, eprot, n_eprot;
 		int level;
 		pgprot_t prot;
+		vmexit_loop_detect(vcpu, gpa);
+		//return grant_all(vcpu, gpa, qual);	
 		/* Bit 8 is cleared if it's a page walk or update of accessed*/
 		if (!(qual & 0x100)){
 			TDEBUG(log_ptr(vcpu),"EPT Page walk violation  "
@@ -4168,6 +4185,7 @@ int handle_EPT_violation(struct vmx_vcpu *vcpu)
 					return grant_all(vcpu, gpa, qual, level);
 				}
 			}
+			BUG_ON(!guest_physical_page_address(gva, &level, &prot));
 			ept_flags_from_prot(prot, &s_flags, &c_flags);
 			eprot = *epte & (EPT_W | EPT_R | EPT_X);
 			/* if already mapped, it's either a change or alias */
@@ -4195,6 +4213,7 @@ int handle_EPT_violation(struct vmx_vcpu *vcpu)
 					BUG();
 				}
 			}
+			return grant_all(vcpu, gpa, qual);	
 			vpid_sync_context(vcpu->vpid);
 			vmx_put_cpu(vcpu);
 			return 1;
@@ -4238,6 +4257,7 @@ int handle_EPT_violation(struct vmx_vcpu *vcpu)
 				HLOG("Kernel space EPT Violation gpa %#lx "
 				       "va %#lx set %#lx clear %#lx\n",
 				       gpa, gva, s_flags, c_flags);
+				//return grant_all(vcpu, gpa, qual);	
 				vpid_sync_context(vcpu->vpid);
 				vmx_put_cpu(vcpu);
 				return 1;
