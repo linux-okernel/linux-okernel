1dbd6ce4ad00d8c24925d61806816acdc3840415
Nigel Edwards <nigel.edwards@hpe.com>
2017-06-07 08:33:14 +0100

    Add additional metrics and atomic variable refresh_needed to control refresh of global cache. Adjust global cache size based on number of CPUs.

--- arch/x86/okernel/okmm.h
+++ arch/x86/okernel/okmm.h
@@ -1,13 +1,21 @@
 #ifndef OKMM_H
 #define OKMM_H
 
-#define OKMM_INIT_NR 128 /* Initial number of entries */
-#define OKMM_MIN_PERCPU 64 /* Mininum initial number in per CPU cache */
+//#define OKMM_INIT_NR 128 /* Initial number of entries */
+#define OKMM_INIT_NR (1 << 16) /* Initial number of entries */
+//#define OKMM_MIN_PERCPU 64 /* Mininum initial number in per CPU cache */
+#define OKMM_MIN_PERCPU (1 << 8) /* Mininum initial number in per CPU cache */
+#define OKMM_PERCPU (1 << 8) /* Mininum initial number in per CPU cache */
+
+// For perf test on 640
+//#define OKMM_MIN_PERCPU (1 << 14) /* Mininum initial number in per CPU cache */
 
  /* Miniumum number of entries in Global backing cache */
-#define GC_MIN (OKMM_MIN_PERCPU * 2)
+//#define GC_MIN (OKMM_MIN_PERCPU * 2)
+#define GC_MIN  (1 << 10)
 /* Step by which to grow the cache if we go below GC_MIN*/
-#define GC_STEP OKMM_MIN_PERCPU
+//#define GC_STEP OKMM_MIN_PERCPU
+#define GC_STEP (1 << 8)
 
 struct ok_pt_cache_entry {
 	struct list_head list;
