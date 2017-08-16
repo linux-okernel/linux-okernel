83ce9450ce04d501e5a0724fd655c43e46dee27a
Nigel Edwards <nigel.edwards@hpe.com>
2017-08-16 09:56:25 +0100

    Export set_memory_rw. This might be more useable for updating linux_banner

--- arch/x86/okernel/oktest.c
+++ arch/x86/okernel/oktest.c
@@ -7,6 +7,7 @@
 #include<asm-generic/sections.h>
 #include<asm/text-patching.h>
 
+extern int set_memory_rw(unsigned long addr, int numpages);
 extern void *text_poke(void *addr, const void *opcode, size_t len);
 void oktargets(unsigned long (*address)[])
 {
@@ -15,18 +16,10 @@ void oktargets(unsigned long (*address)[])
 	(*address)[1] = (unsigned long)_etext;
 	(*address)[2] = (unsigned long)&__start_rodata;
 	(*address)[3] = (unsigned long)&__end_rodata;
-	(*address)[4] = (unsigned long)text_poke;
-	(*address)[5] = (unsigned long)&text_mutex;
-	(*address)[6] = (unsigned long)linux_banner;
-	p = vmalloc_to_page((void *) linux_banner);
-	printk("okernel linux_banner page is %#lx\n", (unsigned long) p);
-	p = vmalloc_to_page((void *) &linux_banner);
-	printk("okernel &linux_banner page is %#lx\n", (unsigned long) p);
-	p = vmalloc_to_page((void *)text_poke);
-	printk("okernel text_poke page is %#lx\n", (unsigned long) p);
-	p = vmalloc_to_page((void *)_etext);
-	printk("okernel _etext page is %#lx\n", (unsigned long) p);
-	
+	(*address)[4] = (unsigned long)set_memory_rw;
+	(*address)[5] = (unsigned long)linux_banner;
+	(*address)[6] = (unsigned long)text_poke;
+	(*address)[7] = (unsigned long)&text_mutex;
 }
 
 EXPORT_SYMBOL_GPL(oktargets);
