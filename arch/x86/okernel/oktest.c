1bcd69e236322309dd65b055d36779879e450be5
Nigel Edwards <nigel.edwards@hpe.com>
2017-08-16 16:31:00 +0100

    Added additional functions

--- arch/x86/okernel/oktest.c
+++ arch/x86/okernel/oktest.c
@@ -8,18 +8,19 @@
 #include<asm/text-patching.h>
 
 extern int set_memory_rw(unsigned long addr, int numpages);
+extern void flush_tlb_all(void);
 extern void *text_poke(void *addr, const void *opcode, size_t len);
 void oktargets(unsigned long (*address)[])
 {
-	struct page *p;
 	(*address)[0] = (unsigned long)_text;
 	(*address)[1] = (unsigned long)_etext;
 	(*address)[2] = (unsigned long)&__start_rodata;
 	(*address)[3] = (unsigned long)&__end_rodata;
 	(*address)[4] = (unsigned long)set_memory_rw;
-	(*address)[5] = (unsigned long)linux_banner;
-	(*address)[6] = (unsigned long)text_poke;
-	(*address)[7] = (unsigned long)&text_mutex;
+	(*address)[5] = (unsigned long)flush_tlb_all;
+	(*address)[6] = (unsigned long)linux_banner;
+	(*address)[7] = (unsigned long)text_poke;
+	(*address)[8] = (unsigned long)&text_mutex;
 }
 
 EXPORT_SYMBOL_GPL(oktargets);
